e18d326219fb260d37c96c0b082eadf10938dd64
elainedias16 <elainediaspires16@gmail.com>
2022-06-02 16:39:42 -0300

    adicionado timeout e validacao do tamanho do pacote

--- TP2/client.py
+++ TP2/client.py
@@ -2,17 +2,31 @@ from socket import *
 
 serverName = 'localhost'
 serverPort = 30000
+
 buffer_size = 10
+package_size = 30
+package_lost = 0
 
 #Create a UDP socket
-clientSocket = socket(AF_INET, SOCK_DGRAM)  
+clientSocket = socket(AF_INET, SOCK_DGRAM) 
+clientSocket.settimeout(1)  #tempo maximo de espera por uma resposta do server
 
-message = input("Input what you want to send to the server: ")
+msg_to_server = 'ping'
+for i in range(1,11):
+    if(len(msg_to_server) > package_size):
+        print('Invalid message!')
+        exit(1)
 
-clientSocket.sendto(message.encode(),(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(buffer_size)
+    else:
+        #Sending message to server
+        clientSocket.sendto(msg_to_server .encode(),(serverName, serverPort))
+        try:
+            msg_from_server, serverAddress = clientSocket.recvfrom(buffer_size)
+            print(msg_from_server.decode())
+        except clientSocket.Timeouterror:
+            package_lost+= package_lost
+            print('Unfortunately the package has been lost...')
 
-print(modifiedMessage.decode())
 
+print('number of package lost: ' ,package_lost)
 clientSocket.close()
-
