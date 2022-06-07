62291cd788d801137f8c85874277478205f9cc11
elainedias16 <elainediaspires16@gmail.com>
2022-06-07 16:41:45 -0300

    dicionario finalizado, fazer mais testes

--- TP2/client.py
+++ TP2/client.py
@@ -15,39 +15,45 @@ package_sent = 0
 package_received = 0
 percent_lost = 0
 
-rtt = []
-# rtt2 = []
 num_pings = 10
+
+#Every position of the dictionary bellow will receive two values, one related to ping 
+# and another related to pong, if there isn't any loss of packages.
+#  The key is the number of the package.
 dic_time = {}
+#List that will receive rtt's times
+rtt = []
 
 #Create a UDP socket
 clientSocket = socket(AF_INET, SOCK_DGRAM) 
 clientSocket.settimeout(2)  #Maximum time of waiting
 
-def rtt_medio(rtt):
-    list_sum = sum(rtt)
-    media = list_sum / num_pings 
+def rtt_medio(rtt, size):
+    rtt_sum = sum(rtt)
+    #media = rtt_sum / num_pings 
+    media = rtt_sum / size
     return media
 
-def standard_deviation(rtt, media):
+def standard_deviation(rtt, media, size):
     sum = 0
-    for i in range(0,num_pings): 
+    for i in range(0,size): 
         sum += (rtt[i] - media ) ** 2
-    sd = sum/num_pings
+    #sd = sum/num_pings
+    sd = sum/ size
     sd = math.sqrt(sd)
     return sd
 
-#calculo do rtt a partir do dicionario
-def rtt2(dic_time):
-    rtt2 = []
+
+def rtt_calc(dic_time, rtt):
     for num_seq in dic_time:
-        start = dic_time[num_seq][0]
-        print(start)
-        end = dic_time[num_seq][1]
-        print(end)
-        rtt2_value = end- start
-        rtt2.append(rtt2_value)
-    print(rtt2)
+        #print(len(dic_time[num_seq]))
+        if(len(dic_time[num_seq])==2): #if there is time of ping and pong
+            start = dic_time[num_seq][0]
+            end = dic_time[num_seq][1]
+            rtt_value = end - start
+            rtt.append(rtt_value)
+    #print(rtt2)
+
 
 for i in range(1,11):
     starttime = time.time()
@@ -58,8 +64,7 @@ for i in range(1,11):
     #Header 
     msg_to_server =  num_seq + '0' + timestamp_str + 'Elaine'
 
-   
-    dic_time[num_seq] =  [] #Every position of the dictionary will recevif two value, one of ping and another of pong
+    dic_time[num_seq] =  [] 
     dic_time[num_seq].append(starttime)
   
     if(len(msg_to_server) > package_size):
@@ -87,28 +92,27 @@ for i in range(1,11):
             if(index == num_seq):
                 dic_time[num_seq].append(endtime)
 
-              
-
-            #RTT time
-            rtt.append(endtime - starttime)
 
         except timeout:
             package_lost += 1
-            print('Unfortunately this package has been lost...')
-            rtt.append(0)
+            print('Unfortunately this message has been lost...')
+            #rtt.append(0)
             
 
 
 
 clientSocket.close()
 end_exec = time.time()
+rtt_calc(dic_time, rtt)
+size = len(rtt) #Not necessarily will be the number of pings, because some package maybe has been lost.
+print('size: ',  size)
 
 #Statistics
-media = rtt_medio(rtt)  * pow(10, 3) #Converting to ms  
-sd = standard_deviation(rtt, media) #mdev
+media = rtt_medio(rtt, size)  * pow(10, 3) #Converting to ms  
+sd = standard_deviation(rtt, media, size) #mdev
 rtt.sort()
 rtt_min = rtt[0] * pow(10, 3) #Converting to ms
-rtt_max= rtt[num_pings - 1]  * pow(10, 3) #Converting to ms
+rtt_max= rtt[size - 1]  * pow(10, 3) #Converting to ms
 percent_lost = (package_lost / package_sent ) * 100
 time_exec = (end_exec - start_exec) * pow(10, 3) #Converting to ms
 time_exec = math.trunc(time_exec)
@@ -121,9 +125,6 @@ print('rtt min/avg/max/mdev = {:03f}/{:03f}/{:03f}/{:03f}'
  .format(rtt_min, media, rtt_max, sd))
 
 
-print(dic_time) #ok
-print('--------')
-rtt2(dic_time)
 
 
 
