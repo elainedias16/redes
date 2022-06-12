b4e050b9785a8074e2217c4234c2f4b9b83c85a2
elainedias16 <elainediaspires16@gmail.com>
2022-06-12 16:08:48 -0300

    trabalho finalizado

--- TP2/client.py
+++ TP2/client.py
@@ -5,7 +5,7 @@ import time
 
 start_exec = time.time()
 
-serverName = 'localhost'
+serverName = '168.227.188.22'
 serverPort = 30000
 
 buffer_size = 40
@@ -27,7 +27,7 @@ rtt = []
 
 #Create a UDP socket
 clientSocket = socket(AF_INET, SOCK_DGRAM) 
-clientSocket.settimeout(2)  #Maximum time of waiting
+clientSocket.settimeout(1)  #Maximum time of waiting
 
 def rtt_medio(rtt, size):
     rtt_sum = sum(rtt)
@@ -64,36 +64,54 @@ for i in range(1,11):
     dic_time[num_seq].append(starttime)
   
     if(len(msg_to_server) > package_size):
-        print('Invalid message!')
+        print('Package size error!')
+        package_lost += 1
         continue
 
     else:
         #Sending message to server
         clientSocket.sendto(msg_to_server .encode(),(serverName, serverPort))
+        print('Message to server:   ' , msg_to_server)
         package_sent += 1
         try:
             msg_from_server, serverAddress = clientSocket.recvfrom(buffer_size)
             msg_from_server = msg_from_server.decode()
             endtime = time.time()
-            package_received += 1
 
             #Checking if the header received from server is valid
             index_int = int(msg_from_server[0:5])
             m3= int(len(msg_from_server[6:10]))
-            if(msg_from_server[5] == 0 or index_int > 10 or len(msg_from_server[10:]) > 30 or m3 !=4): 
-                print('Invalid message!')
+            if(index_int > 10):
+                print('Sequence number error!')
+                package_lost += 1
+                continue
+            elif(msg_from_server[5] != '1'):
+                print('Ping/Pong error!')
+                package_lost += 1
+                continue
+            elif(m3 != 4 ):
+                print('Timestamp error!')
+                package_lost += 1
+                continue
+            elif(len(msg_from_server[10:]) > 30):
+                print('Message size error!')
+                package_lost += 1
+                continue
             else:
+                package_received += 1
                 print('Message from server: ' , msg_from_server)
 
             #Checking if the pong is comming from the respective ping
             index = msg_from_server[0:5]
             if(index == num_seq):
                 dic_time[num_seq].append(endtime)
-
+                if(endtime - starttime > 1):
+                    package_lost -= 1
+            
 
         except timeout:
             package_lost += 1
-            print('Unfortunately this message has been lost...')
+            print('Timeout...')
            
             
 
@@ -110,7 +128,7 @@ if(size > 0):
 
     rtt_min = rtt[0] * pow(10, 3) #Converting to ms
     rtt_max= rtt[size - 1]  * pow(10, 3) #Converting to ms
-    percent_lost = (package_lost / package_sent ) * 100
+    percent_lost = (package_lost  / 10 ) * 100
     time_exec = (end_exec - start_exec) * pow(10, 3) #Converting to ms
     time_exec = math.trunc(time_exec)
 
